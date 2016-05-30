b740f1efbf63e5692b56302fce70191b9b63ed65
Igor <Malheiros@MacBook-Pro-de-Igor.local>
2016-05-30 16:14:22 +0100

    Initialization, Compute Fitness, Roullete and Copy first version

--- GA.py
+++ GA.py
@@ -1,4 +1,4 @@
-from random import randint
+import random
 
 #Class of individuals
 class Individual:
@@ -10,26 +10,69 @@ class Individual:
 	def __str__(self):
 		return "Individual: %s --- Score: %d" % (self.gene, self.score)
 
-	def setScore(self, objFucntion):
-		self.score = objFucntion
-		
+	def __radd__(self, other):
+		return other + self.score
+
+	def setScore(self, objFunction):
+		self.score = objFunction(self.gene)
+
 #Class of Genetic Algorithm
 class GA:
 	#
-	def __init__(self, geneSize, populationSize, variability, mutation):
+	def __init__(self, geneSize, populationSize, variability, mutation, fraction, objFunction):
 		self.geneSize = geneSize
 		self.populationSize = populationSize
 		self.variability = variability
-		self.mutation = mutation 
+		self.mutation = mutation
+		self.fraction = fraction
+		self.objFunction = objFunction
+		self.totalScore = 0
 
 	def generateIndividual(self):
 		gene = ""
 		for i in xrange(self.geneSize):
-			gene += str(randint(1, self.variability))
+			gene += str(random.randint(1, self.variability))
 		return Individual(gene)
 
 	def initializePopulation(self):
-		return [generateIndividual(self.geneSize, self.variability) for x in xrange(self.populationSize)]
+		return [self.generateIndividual() for x in xrange(self.populationSize)]
 
-test = GA(20, 4, 4, 0)
-print(test.generateIndividual())
+	def computeFitness(self, population):
+		for i in population:
+			i.setScore(self.objFunction)
+		return population
+
+	def roulleteSelection(self, population):
+		totalScore = sum(population)
+		r = random.random()
+		total = 0.0
+		for i in population:
+			total += (i.score/float(totalScore))
+			if(r < total):
+				return i
+
+	def copy(self, population):
+		nCopy = int((1-self.fraction) * self.populationSize)
+		copyPop = []
+		for i in xrange(nCopy):
+			indiv = self.roulleteSelection(population)
+			copyPop.append(indiv)
+			population.remove(indiv)
+
+		return copyPop
+
+#Test section
+
+#Objective Function definition
+def objFunction(string):
+	return sum(int(x) for x in string)
+
+test = GA(20, 4, 4, 0, 0.3, objFunction)
+pop = test.initializePopulation()
+pop = test.computeFitness(pop)
+print("------Start Generation------")
+for p in pop:
+	print(p)
+print("-------Copy---------")
+for p in test.copy(pop):
+	print(p)
